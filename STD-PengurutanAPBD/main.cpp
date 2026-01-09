6e043ed33c05558d43c944ac733b1a9c074cc129
takeadih <razkarabbanynurdiyana@gmail.com>
2026-01-09 14:38:51 +0700

    Update main.cpp

--- STD-PengurutanAPBD/main.cpp
+++ STD-PengurutanAPBD/main.cpp
@@ -7,54 +7,30 @@ int main() {
     createTree(root);
     loadSampleAPBD(root);
 
-    int pilihan;
+    int pilih;
     do {
-        cout << "\n=== SISTEM TREE HIRARKI APBD ===\n";
+        cout << "\n=== TREE HIRARKI APBD ===\n";
         cout << "1. Tampilkan Struktur Tree\n";
-        cout << "2. Traversal Preorder\n";
-        cout << "3. Traversal Inorder\n";
-        cout << "4. Traversal Postorder\n";
+        cout << "2. Preorder Traversal\n";
+        cout << "3. Inorder Traversal\n";
+        cout << "4. Postorder Traversal\n";
         cout << "5. Anggaran Terkecil\n";
         cout << "6. Anggaran Terbesar\n";
         cout << "0. Keluar\n";
         cout << "Pilih: ";
-        cin >> pilihan;
+        cin >> pilih;
 
-        switch (pilihan) {
-            case 1:
-                tampilTree(root);
-                break;
-            case 2:
-                cout << "Preorder: ";
-                preorder(root);
-                cout << endl;
-                break;
-            case 3:
-                cout << "Inorder: ";
-                inorder(root);
-                cout << endl;
-                break;
-            case 4:
-                cout << "Postorder: ";
-                postorder(root);
-                cout << endl;
-                break;
-            case 5:
-                cout << "Anggaran terkecil: Rp "
-                     << getMinAnggaran(root) << " M\n";
-                break;
-            case 6:
-                cout << "Anggaran terbesar: Rp "
-                     << getMaxAnggaran(root) << " M\n";
-                break;
-            case 0:
-                cout << "Keluar dari sistem.\n";
-                break;
-            default:
-                cout << "Menu tidak tersedia.\n";
+        switch (pilih) {
+            case 1: tampilTree(root); break;
+            case 2: cout << "Preorder: "; preorder(root); cout << endl; break;
+            case 3: cout << "Inorder: "; inorder(root); cout << endl; break;
+            case 4: cout << "Postorder: "; postorder(root); cout << endl; break;
+            case 5: cout << "Anggaran terkecil: Rp " << getMinAnggaran(root) << " M\n"; break;
+            case 6: cout << "Anggaran terbesar: Rp " << getMaxAnggaran(root) << " M\n"; break;
+            case 0: cout << "Keluar.\n"; break;
+            default: cout << "Menu tidak tersedia.\n";
         }
-
-    } while (pilihan != 0);
+    } while (pilih != 0);
 
     return 0;
 }
