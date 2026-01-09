6a3b22e39d522abc32c8e17c4bd954e07409fea4
takeadih <razkarabbanynurdiyana@gmail.com>
2026-01-09 13:43:54 +0700

    Update main.cpp

--- STD-PengurutanAPBD/main.cpp
+++ STD-PengurutanAPBD/main.cpp
@@ -5,110 +5,63 @@ using namespace std;
 int main() {
     adrNode root;
     createTree_APBD(root);
+    loadSampleAPBD(root);
 
     int pilihan;
-
     do {
-        cout << "\n=== SISTEM PENGURUTAN APBD BERDASARKAN UNIQUE ID ===\n";
-        cout << "1. Tambah Program APBD\n";
-        cout << "2. Cari Program APBD\n";
-        cout << "3. Tampilkan APBD Terurut (Inorder)\n";
-        cout << "4. Tampilkan Preorder\n";
-        cout << "5. Tampilkan Postorder\n";
-        cout << "6. Jumlah Program APBD\n";
-        cout << "7. Total Anggaran APBD\n";
-        cout << "8. Unique ID Terkecil\n";
-        cout << "9. Unique ID Terbesar\n";
+        cout << "\n=== SISTEM PENGURUTAN APBD (BST) ===\n";
+        cout << "1. Cari Program APBD\n";
+        cout << "2. Tampilkan Inorder (Terurut)\n";
+        cout << "3. Tampilkan Preorder\n";
+        cout << "4. Tampilkan Postorder\n";
+        cout << "5. Jumlah Program APBD\n";
+        cout << "6. Total Anggaran APBD\n";
+        cout << "7. Anggaran Terkecil\n";
+        cout << "8. Anggaran Terbesar\n";
         cout << "0. Keluar\n";
         cout << "Pilih: ";
         cin >> pilihan;
 
         switch (pilihan) {
-
-        case 1: {
-            int id;
-            string prov, kab, nama, jenis;
-            double anggaran;
-
-            cout << "Unique ID: ";
-            cin >> id;
-
-            if (!isUniqueID_APBD(root, id)) {
-                cout << "ID sudah digunakan.\n";
+            case 1: {
+                int id;
+                cout << "Masukkan Unique ID: ";
+                cin >> id;
+                adrNode p = searchNode_APBD(root, id);
+                if (p) {
+                    cout << "Kab/Kota : " << p->kabupatenKota << endl;
+                    cout << "Program  : " << p->namaProgram << endl;
+                    cout << "Anggaran : Rp " << p->anggaran << " M\n";
+                } else {
+                    cout << "Data tidak ditemukan.\n";
+                }
                 break;
             }
-
-            cin.ignore();
-            cout << "Provinsi: ";
-            getline(cin, prov);
-            cout << "Kabupaten/Kota: ";
-            getline(cin, kab);
-            cout << "Nama Program: ";
-            getline(cin, nama);
-            cout << "Jenis Belanja: ";
-            getline(cin, jenis);
-            cout << "Anggaran (juta): ";
-            cin >> anggaran;
-
-            insertNode_APBD(root,
-                createNode_APBD(id, prov, kab, nama, jenis, anggaran));
-
-            cout << "Data APBD berhasil ditambahkan.\n";
-            break;
+            case 2: tampilInorder_APBD(root); break;
+            case 3: tampilPreorder_APBD(root); break;
+            case 4: tampilPostorder_APBD(root); break;
+            case 5:
+                cout << "Jumlah program APBD: "
+                     << countProgram_APBD(root) << endl;
+                break;
+            case 6:
+                cout << "Total anggaran APBD: Rp "
+                     << totalAnggaran_APBD(root) << " M\n";
+                break;
+            case 7:
+                cout << "Anggaran terkecil: Rp "
+                     << getMinAnggaran_APBD(root) << " M\n";
+                break;
+            case 8:
+                cout << "Anggaran terbesar: Rp "
+                     << getMaxAnggaran_APBD(root) << " M\n";
+                break;
+            case 0:
+                cout << "Keluar dari sistem.\n";
+                break;
+            default:
+                cout << "Menu tidak tersedia.\n";
         }
-
-        case 2: {
-            int id;
-            cout << "Masukkan Unique ID: ";
-            cin >> id;
-
-            adrNode p = searchNode_APBD(root, id);
-            if (p != nullptr) {
-                cout << p->provinsi << " - " << p->kabKota << endl;
-                cout << p->namaProgram << endl;
-                cout << p->jenisBelanja << endl;
-                cout << "Rp " << p->anggaran << " M\n";
-            } else {
-                cout << "Data tidak ditemukan.\n";
-            }
-            break;
-        }
-
-        case 3:
-            tampilUrutAPBD_Inorder(root);
-            break;
-
-        case 4:
-            tampilAPBD_Preorder(root);
-            break;
-
-        case 5:
-            tampilAPBD_Postorder(root);
-            break;
-
-        case 6:
-            cout << "Jumlah program: "
-                 << countProgram_APBD(root) << endl;
-            break;
-
-        case 7:
-            cout << "Total anggaran: Rp "
-                 << totalAnggaran_APBD(root) << " M\n";
-            break;
-
-        case 8:
-            if (root != nullptr)
-                cout << "ID terkecil: "
-                     << getMinID_APBD(root) << endl;
-            break;
-
-        case 9:
-            if (root != nullptr)
-                cout << "ID terbesar: "
-                     << getMaxID_APBD(root) << endl;
-            break;
-        }
-
     } while (pilihan != 0);
 
     return 0;
