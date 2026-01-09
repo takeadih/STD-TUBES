b42a03ae8bfa3c90fe89bef01f9a69b9bca6fc45
takeadih <razkarabbanynurdiyana@gmail.com>
2026-01-09 13:22:52 +0700

    Update Tree.h

--- STD-PengurutanAPBD/Tree.h
+++ STD-PengurutanAPBD/Tree.h
@@ -15,31 +15,41 @@ typedef int infotype;
 typedef struct Node* adrNode;
 
 struct Node {
-    infotype idAPBD;        // UNIQUE ID (key pengurutan)
+    infotype idAPBD;            // UNIQUE ID (key BST)
+    string provinsi;
+    string kabKota;
     string namaProgram;
     string jenisBelanja;
     double anggaran;
+
     adrNode left;
     adrNode right;
 };
 
-
+/* Operasi dasar BST */
 void createTree_APBD(adrNode &root);
+adrNode createNode_APBD(
+    infotype id,
+    string prov,
+    string kabkota,
+    string nama,
+    string jenis,
+    double anggaran
+);
 
-adrNode createNode_APBD(infotype id, string nama, string jenis, double anggaran);
 bool isUniqueID_APBD(adrNode root, infotype id);
-
 void insertNode_APBD(adrNode &root, adrNode p);
 adrNode searchNode_APBD(adrNode root, infotype id);
 
-// Hasil pengurutan APBD berdasarkan UNIQUE ID
-void tampilUrutAPBD_Inorder(adrNode root);
+/* Traversal */
+void tampilUrutAPBD_Inorder(adrNode root);   // hasil pengurutan
+void tampilAPBD_Preorder(adrNode root);
+void tampilAPBD_Postorder(adrNode root);
 
+/* Fitur tambahan */
 int countProgram_APBD(adrNode root);
 double totalAnggaran_APBD(adrNode root);
 infotype getMinID_APBD(adrNode root);
 infotype getMaxID_APBD(adrNode root);
-
-void loadSampleAPBD(adrNode &root);
 
 #endif
